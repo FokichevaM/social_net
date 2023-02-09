20e8d055c5c21899832a84975580abfbf3a15b29
Anatoliy Kovalev <anatoliy@vibodj.com>
2023-02-09 23:09:21 +0200

    sssssssssssssssss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+// import LoginPage from './pages/LoginPage';
 
 function App() {
   return <div>'Пісюн'</div>;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './resources/styles/main.scss';
+// import './resources/styles/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
