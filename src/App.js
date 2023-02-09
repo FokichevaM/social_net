e986f278a0a93f8f6f8cef378fff5384198dccf3
Anatoliy Kovalev <anatoliy@vibodj.com>
2023-02-09 22:10:36 +0200

    pisyn

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return <div>'Пісюн'</div>;
+  // <Router>
+  //   <Routes>
+  //     <Route path="/" element={<LoginPage />} />
+  //   </Routes>
+  // </Router>
+}
+
+export default App;
