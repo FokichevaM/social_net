e7653c1bc1b0b9640e3fcf25c291f976d441e7bc
Anatoliy Kovalev <anatoliy@vibodj.com>
2023-02-09 22:15:27 +0200

    s

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './resources/styles/main.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+const render = (App) =>
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+render(App);
