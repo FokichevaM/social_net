c6723315e77c2a1d295038b90867ca27339b2757
Anatoliy Kovalev <anatoliy@vibodj.com>
2023-02-09 23:04:19 +0200

    ssssssssssssssss

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './resources/styles/main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// const render = (App) =>
+//   ReactDOM.render(<App />, document.getElementById('root'));
+
+// render(App);
